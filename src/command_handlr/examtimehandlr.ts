1e1dd314eb4474c63b97b5a4d3bcbbf9f649d422
Azraf Al Monzim <dev@monzim.com>
2023-07-05 23:57:30 +0600

    #latest Summer 23 Info Added CSE, EEE, BBA

--- src/command_handlr/examtimehandlr.ts
+++ src/command_handlr/examtimehandlr.ts
@@ -1,94 +1,94 @@
-import { CacheType, ChatInputCommandInteraction } from "discord.js";
+import {
+  CacheType,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+} from "discord.js";
+import { sendWebhookErrorMessage } from "../webhook/send_message";
+import { findCoursesByDepartmentCourseCodeAndSection } from "../cosmos/cosmos_search";
 
-export const examTimeHandlr = async (
-  interaction: ChatInputCommandInteraction<CacheType>
-) => {
-  await interaction
-    .reply({
-      content: `Opps! Currently, no exam is scheduled. It will be updated as soon as the exam schedule is published.`,
-    })
-    .catch((err) => {
-      console.log("🚀 ~ file: examtimehandlr.ts:11 ~ err:", err);
-    });
-};
-
-// const department: string =
-//   interaction.options.getString("department") ?? "";
-
-// const courseCode: string =
-//   interaction.options.getString("course") ?? "";
-
-// const section: string = interaction.options.getString("section") ?? "";
-
-// await interaction.deferReply().catch((err) => {
-//   console.log("🚀 ~ file: index.ts:199 ~ bot.on ~ err:", err);
-//   sendWebhookErrorMessage("index.ts:199", err);
-
-//   interaction.followUp("Unknown command").catch((err) => {
-//     console.log("🚀 ~ file: index.ts:204 ~ bot.on ~ err:", err);
-//     sendWebhookErrorMessage("index.ts:204", err);
-//   });
-// });
-
-// const courses = await findCoursesByDepartmentCourseCodeAndSection(
-//   department.toUpperCase(),
-//   courseCode.toUpperCase(),
-//   section.toUpperCase()
-// );
-
-// if (courses.length === 0) {
-//   console.log("No courses found");
+// export const examTimeHandlr = async (
+//   interaction: ChatInputCommandInteraction<CacheType>
+// ) => {
 //   await interaction
-//     .followUp({
-//       content: `${interaction.user}  No courses found for ${courseCode} ${section}`,
+//     .reply({
+//       content: `Opps! Currently, no exam is scheduled. It will be updated as soon as the exam schedule is published.`,
 //     })
 //     .catch((err) => {
-//       console.log("🚀 ~ file: index.ts:223 ~ bot.on ~ err:", err);
-//       sendWebhookErrorMessage("index.ts:223", err);
-
-//       interaction.followUp("Unknown command").catch((err) => {
-//         console.log("🚀 ~ file: index.ts:228 ~ bot.on ~ err:", err);
-//         sendWebhookErrorMessage("index.ts:228", err);
-//       });
+//       console.log("🚀 ~ file: examtimehandlr.ts:11 ~ err:", err);
 //     });
-//   return;
-// }
+// };
 
-// let listEm: any = [];
+export const examTimeHandlrProd = async (
+  interaction: ChatInputCommandInteraction<CacheType>
+) => {
+  const department: string = interaction.options.getString("department") ?? "";
+  const courseCode: string = interaction.options.getString("course") ?? "";
+  const section: string = interaction.options.getString("section") ?? "";
 
-// courses.map((course) => {
-//   let _selectedSection = course.Section.toLowerCase();
-//   _selectedSection = _selectedSection.replace(/\s/g, "");
+  await interaction.deferReply().catch((err) => {
+    sendWebhookErrorMessage("index.ts:199", err);
 
-//   if (_selectedSection === section.toLowerCase()) {
-//     listEm.push(
-//       new EmbedBuilder()
-//         .setTitle(course.CourseCode)
-//         .setColor("Random")
-//         .setDescription(
-//           `Section: ${course.Section}     Faculty: ${course.Teacher}\n**${course.ExamDate} at ${course.ExamTime}**\n${course.Room}\n`
-//         )
-//     );
-//   }
-// });
+    interaction.followUp("Unknown command").catch((err) => {
+      sendWebhookErrorMessage("index.ts:204", err);
+    });
+  });
 
-// let title = `In total, the query has **${courses.length}** ${
-//   courses.length > 1 ? "courses" : "course"
-// }. The following **${listEm.length}** ${
-//   listEm.length > 1 ? "courses" : "course"
-// } match. :)`;
+  const courses = await findCoursesByDepartmentCourseCodeAndSection(
+    department.toUpperCase(),
+    courseCode.toUpperCase(),
+    section.toUpperCase()
+  );
 
-// await interaction
-//   .followUp({
-//     content: `${interaction.user} **${courses[0].CourseTitle}**\n ${title}`,
-//     embeds: listEm,
-//   })
-//   .catch((err) => {
-//     console.log("🚀 ~ file: index.ts:261 ~ bot.on ~ err:", err);
-//     sendWebhookErrorMessage("index.ts:261", err);
+  if (courses.length === 0) {
+    console.log("No courses found");
+    await interaction
+      .followUp({
+        content: `${interaction.user}  No courses found for ${courseCode} ${section}`,
+      })
+      .catch((err) => {
+        sendWebhookErrorMessage("index.ts:223", err);
 
-//     interaction.followUp("Unknown command").catch((err) => {
-//       console.log("🚀 ~ file: index.ts:267 ~ bot.on ~ err:", err);
-//       sendWebhookErrorMessage("index.ts:267", err);
-//     });
-//   });
+        interaction.followUp("Unknown command").catch((err) => {
+          sendWebhookErrorMessage("index.ts:228", err);
+        });
+      });
+    return;
+  }
+
+  let listEm: any = [];
+
+  courses.map((course) => {
+    let _selectedSection = course.Section.toLowerCase();
+    _selectedSection = _selectedSection.replace(/\s/g, "");
+
+    if (_selectedSection === section.toLowerCase()) {
+      listEm.push(
+        new EmbedBuilder()
+          .setTitle(course.CourseCode)
+          .setColor("Random")
+          .setDescription(
+            `Section: ${course.Section}     Faculty: ${course.Teacher}\n**${course.ExamDate} at ${course.ExamTime}**\n${course.Room}\n`
+          )
+      );
+    }
+  });
+
+  let title = `In total, the query has **${courses.length}** ${
+    courses.length > 1 ? "courses" : "course"
+  }. The following **${listEm.length}** ${
+    listEm.length > 1 ? "courses" : "course"
+  } match. :)`;
+
+  await interaction
+    .followUp({
+      content: `${interaction.user} **${courses[0].CourseTitle}**\n ${title}`,
+      embeds: listEm,
+    })
+    .catch((err) => {
+      sendWebhookErrorMessage("index.ts:261", err);
+
+      interaction.followUp("Unknown command").catch((err) => {
+        sendWebhookErrorMessage("index.ts:267", err);
+      });
+    });
+};
--- src/command_handlr/versionhandlr.ts
+++ src/command_handlr/versionhandlr.ts
@@ -5,7 +5,7 @@ export const versionHandlr = async (
 ) => {
   await interaction
     .reply({
-      content: `UIU Bot **v.1.0.6**\n **Summer 2023** information. \n**Developer** <@669529872644833290>\n\n**Features**\n- Unizim\n- applink\n- Exam Time\n- Installment\n- Holiday\n- Makeup\n- Exam\n- Version`,
+      content: `UIU Bot **v.1.0.7**\n **Summer 2023** information. \n**Developer** <@669529872644833290>\n\n**Features**\n- Unizim\n- applink\n- Exam Time\n- Installment\n- Holiday\n- Makeup\n- Exam\n- Version`,
     })
     .catch((err) => {
       console.log("🚀 ~ file: versionhandlr.ts:9 ~ err:", err);

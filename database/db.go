d75f53e267641b0070be9154187c4755129a81f1
Azraf Al Monzim <dev@monzim.com>
2024-03-06 17:39:31 +0600

    Fix Docker Build: Update Dockerfile and .gitignore

--- database/db.go
+++ database/db.go
@@ -22,7 +22,7 @@ func NewDatabaseConnection(config *DatabaseConfig) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", config.Host, config.User, config.Password, config.DBname, config.Port, config.SSlMode)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logger.Error),
 	})
 
 	if err != nil {
